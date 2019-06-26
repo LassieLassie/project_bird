dc040f31e12069156a68d4b7f2d825b549a16eeb
LassieLassie <dezzered@gmail.com>
2019-06-27 03:14:58 +0600

    refactored the way script gets path to model file

--- telegram_bot/model.py
+++ telegram_bot/model.py
@@ -6,6 +6,7 @@ import torch.optim as optim
 import torchvision.transforms as transforms
 import torchvision.models as models
 from fastai.vision import load_learner, Image
+from os import path, sep
 
 # В данном классе мы хотим полностью производить всю обработку картинок, которые поступают к нам из телеграма.
 # Это всего лишь заготовка, поэтому не стесняйтесь менять имена функций, добавлять аргументы, свои классы и
@@ -13,7 +14,7 @@ from fastai.vision import load_learner, Image
 class ClassPredictor:
     def __init__(self):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = load_learner("../model/")
+        self.model = load_learner(path.dirname(path.abspath(__file__)) + sep + ".." + sep + "model", "export.pkl")
         self.to_tensor = transforms.ToTensor()
         #self.device = torch.device('cpu')
         #self.model = torch.load('./modelka.pkl', map_location=self.device)
